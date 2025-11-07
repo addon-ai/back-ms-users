adc6c2a3adb3151e8dea2511a06718012d944ed0
Jiliar Silgado <Jiliar.Silgado.-ND@disney.com>
2025-11-07 07:15:02 -0500

    fixings

--- projects/back-ms-users/init-db.sql
+++ projects/back-ms-users/init-db.sql
@@ -1,3 +1,2 @@
--- Create database if it doesn't exist
-SELECT 'CREATE DATABASE back_ms_users_db'
-WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'back_ms_users_db')\gexec
+-- Create database
+CREATE DATABASE back_ms_users_db;
